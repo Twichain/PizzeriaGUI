c7b2032c8d6c1fd7a6285feca0bdba64149e1dae
David <gkim51@students.kennesaw.edu>
2022-07-19 00:17:48 -0400

    added amber background color (small push double checking it work for me)

--- MomAndPopsPizzeria/src/main/java/com/example/momandpopspizzeria/HelloApplication.java
+++ MomAndPopsPizzeria/src/main/java/com/example/momandpopspizzeria/HelloApplication.java
@@ -9,13 +9,16 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+
+        public void start (Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("homePage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1052, 720);
         stage.setTitle("Hello!");
+        scene.getStylesheets().add("style.css");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
